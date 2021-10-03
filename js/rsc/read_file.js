34f62d03a023bfd2c110346806900ebc93c1e169
cesar <mi_buena_vida@hotmail.com>
2021-10-03 08:32:24 -0400

    last update

---
+++ js/rsc/read_file.js
@@ -0,0 +1,18 @@
+'use strict'
+
+class FileFunctions {
+    constructor() {}
+
+    readTextFile(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        }
+        rawFile.send(null);
+    }
+
+}
